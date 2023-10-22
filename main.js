d5b8e0a00b47b172b4c934e07615929c6fb271e2
Chris Weathers <clweathers1@gmail.com>
2023-10-22 18:50:26 -0400

    Initial setup.

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    canvas_updated();
+}
+
+function draw() {
+    
+}
+
+function canvas_updated() {
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    canvas_updated();
+}
